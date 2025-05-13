ce9f3dfc4fff8d4c181f3f69a4bc7978c3e4f9f2
phllp <felipebeiger@gmail.com>
2025-05-13 21:55:40 +0200

    feat: implement Benefit endpoints

--- src/interfaces/routes/index.ts
+++ src/interfaces/routes/index.ts
@@ -9,6 +9,7 @@ import businessCaseRoutes from "./business-case.routes";
 import actorRoutes from "./actor.routes";
 import milestoneRoutes from "./milestone.routes";
 import riskRoutes from "./risk.routes";
+import benefitRoutes from "./benefit.routes";
 
 const router = Router();
 
@@ -26,5 +27,6 @@ router.use("/business-case", businessCaseRoutes);
 router.use("/actor", actorRoutes);
 router.use("/milestone", milestoneRoutes);
 router.use("/risk", riskRoutes);
+router.use("/benefit", benefitRoutes);
 
 export default router;
